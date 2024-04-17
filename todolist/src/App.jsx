bf3adde495535e9f41c908e542ec159b8f879307
Thainá R Zanfolin <102308025+thainazanfolin@users.noreply.github.com>
2024-04-17 11:13:52 -0300

    struture

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,9 +1,62 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // use state permite a renderização após mudança de dados 
+  //armazenando os dados iniciais - set insere os dados, todos consulta os dados
 
-  return  <div>todolist em react</div>
+  const [todos, setTodos] = useState([
+    {
+      id: 1, 
+      text: "Enviar email para Carolina",
+      category: "Trabalho", 
+      isCompleted: false
+    },
+    {
+      id: 2, 
+      text: "Finalizar projeto PI",
+      category: "Faculdade", 
+      isCompleted: false
+    },
+    {
+      id: 3, 
+      text: "Enviar certificado",
+      category: "Trabalho", 
+      isCompleted: false
+    },
+    {
+      id: 4, 
+      text: "Enviar email Paulo",
+      category: "Faculdade", 
+      isCompleted: false
+    },
+
+
+
+  ]) 
+
+  return  <div className='app'>
+    <h1>Sua lista de tarefas</h1>
+    <div className='todo-list'>
+      {todos.map((todo) => (  /*vamos organizar os to-dos em MAP que percorre os itens TO DO para acessar seus dados */
+        <div className="todo">
+          <div className="content"> 
+            <p>{todo.text}</p>
+          <p className="category">
+            ({todo.category})
+          </p>     
+          </div>
+          <div>
+            <button>Feito!</button>
+            <button>X</button>
+          </div>
+        </div>
+      ))} 
+      
+    </div>
+    
+    
+    
+  </div>
   
 }
 
